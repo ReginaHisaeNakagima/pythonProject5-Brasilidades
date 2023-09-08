79c1d0540e5715aa8ab44835a9f6388ac9f50f71
HisaeNakagima <1823289@aluno.univesp.br>
2023-09-08 12:37:06 -0300

    Formatando Datas

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from datetime import datetime, timedelta
 from datas_br import DatasBr
 
 cadastro = DatasBr()
-print(cadastro.dia_semana())
+print(cadastro)
 
+'''
+hoje = datetime.today()
+hoje_formatada = hoje.strftime("%d/%m/%Y %H:%M")
+
+print(hoje)
+print(hoje_formatada)
+print(type(hoje_formatada))
+'''
 
 
 
