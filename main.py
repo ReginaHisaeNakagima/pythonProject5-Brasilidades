26fdafe2f307dd278a24863241daf487159d56ef
HisaeNakagima <1823289@aluno.univesp.br>
2023-09-08 15:39:03 -0300

    Diferença entre datas e timedelta

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from datetime import datetime, timedelta
 from datas_br import DatasBr
 
-cadastro = DatasBr()
-print(cadastro)
-
-'''
-hoje = datetime.today()
-hoje_formatada = hoje.strftime("%d/%m/%Y %H:%M")
-
-print(hoje)
-print(hoje_formatada)
-print(type(hoje_formatada))
-'''
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
 
 
 
