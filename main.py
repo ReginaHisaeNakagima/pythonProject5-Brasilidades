a8891020e1e0b9bc758722286c3e8b1395621bd8
HisaeNakagima <1823289@aluno.univesp.br>
2023-09-08 07:52:35 -0300

    Validando CNPJ

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from Cpf import Cpf
-
-cpf_um = Cpf("15316264754")
-print(cpf_um)
+from cpf_cnpj import CpfCnpj
+from validate_docbr import CNPJ
+#cpf_um = Cpf("15316267454")
+#print(cpf_um)
+exemplo_cnpj = "35379838000112"
+exemplo_cpf = "11111111112"
+#cnpj = CNPJ()
+#print(cnpj.validate(exemplo_cnpj))
+documento = CpfCnpj(exemplo_cpf, 'cpf')
+print(documento)
 
